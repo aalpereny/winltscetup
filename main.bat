c8afae5d433158a2af0ff7f1c3022b188b723742
Alaattin Alperen Yıldırım <145972778+aalpereny@users.noreply.github.com>
2024-06-06 21:22:02 +0300

    Update main.bat

--- main.bat
+++ main.bat
@@ -16,9 +16,9 @@ if '%errorlevel%' NEQ '0' (
 :gotAdmin
     pushd "%CD%"
     CD /D "%~dp0"
-    powershell.exe /c "Invoke-WebRequest -Uri 'https://github.com/aalpereny/winltscetup/raw/main/files/ltsc/skus.zip' -OutFile 'c:\temp\skus.zip'"
-    powershell.exe /c "Expand-Archive -Force c:\temp\skus.zip"
-    powershell.exe /c "Copy-Item -Path c:\temp\skus.zip -Destination %windir%\system32\spp\tokens -Recurse"
+    powershell.exe /c "Invoke-WebRequest -Uri 'https://github.com/aalpereny/winltscetup/raw/main/files/ltsc/skus.zip' -OutFile %temp%\skus.zip"
+    powershell.exe /c "Expand-Archive -Force %temp%\skus.zip -DestinationPath %temp%\skus"
+    powershell.exe /c "Copy-Item -Path %temp%\skus -Destination %windir%\system32\spp\tokens -Recurse"
     cscript.exe %windir%\system32\slmgr.vbs /rilc
     cscript.exe %windir%\system32\slmgr.vbs /upk >nul 2>&1
     cscript.exe %windir%\system32\slmgr.vbs /ckms >nul 2>&1
